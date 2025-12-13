e9f0a310b6d8c19809c0910a2b280096f266f66a
Med~BARKAOUI <mohamedbarkaoui33@gmail.com>
2025-12-13 05:34:38 +0100

    Update server.js
    server.js cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 // Connexion MongoDB
