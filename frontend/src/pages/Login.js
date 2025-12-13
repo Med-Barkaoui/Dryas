b9ebc3256c6da1f573a6a42124e9b6240469a3b1
Med~BARKAOUI <mohamedbarkaoui33@gmail.com>
2025-12-13 01:15:48 +0100

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -32,7 +32,7 @@ export default function Login() {
 
     setIsLoading(true);
     try {
-      const res = await axios.post("/api/auth/login", { email, password });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { email, password });
 
       if (res.data.msg === "MFA sent") {
         localStorage.setItem("dryas_email", email);
